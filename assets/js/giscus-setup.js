7e67a124cb5181b986b7c054335773550e2ae19f
laracbusby8 <laracbusby@gmail.com>
2026-02-12 19:39:17 +0000

    Deploying to gh-pages from @ laracbusby8/laracbusby8.github.io@645a4117b246a2529fca51956e7760af35e95278 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "laracbusby8/website",
+    "data-repo": "laracbusby8/laracbusby8.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
